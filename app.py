97bf9faa024286c359c762c6b25f3e8ba5629117
sebastianl0315 <sebastianl0315@gmail.com>
2026-06-11 19:48:43 -0500

    Cookies 6

--- app.py
+++ app.py
@@ -404,20 +404,16 @@ def main():
                         res = login_user(email, password)
                         if res:
                             st.session_state.user = res.user
-                            st.session_state.cookies_initialized = True # Evita que el validador de F5 espere tras el login manual
+                            st.session_state.cookie_checked = True # <-- CAMBIAR AQUÍ
             
-                            # Guardamos los tokens en una cookie que dure 30 días
                             session_data = {
                                 "access_token": res.session.access_token,
                                 "refresh_token": res.session.refresh_token
                             }
             
-                            # Protegemos la escritura de la cookie contra desincronizaciones del JS interno
                             try:
                                 controller.set("sb_session", session_data, expires=datetime.datetime.now() + datetime.timedelta(days=30))
                             except TypeError:
-                                # Si el componente de cookies no está listo para escribir en este milisegundo,
-                                # se ignora el error de forma segura. El st.session_state.user ya está asignado.
                                 pass
                 
                             st.rerun()
